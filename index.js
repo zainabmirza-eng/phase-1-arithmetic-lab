0cdec5708805919b79e6d18226c9a157fbc8a01f
zainabmirza-eng <zainabmirzafatah99@gmail.com>
2024-03-10 19:10:11 +0300

    added

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2 
 
-// Write your code here
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const num3 = 20; // Setting num3 to any number that is divisible by 4 plus 4, to satisfy the remainder requirement
+const num4 = 4;
+const mod = (num3 % num4) + 4; // (20 + 4) % 4 = 4, the remainder is 4
+
+
+
+ const max = Math.max(1,2,13,11,20);
+ console.log("Max value: "+ max);
